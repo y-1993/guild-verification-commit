7db92b0adce9d31208b74b8dedcb0cce138b48d3
y-1993 <155223521+y-1993@users.noreply.github.com>
2026-03-05 21:28:00 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:40:28.594Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T18:28:00.103Z
+Commit: Patch API endpoint error handling
