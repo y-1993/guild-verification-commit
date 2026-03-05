d69349a87f4a7fbb4969c549a2292597cb036e1f
y-1993 <155223521+y-1993@users.noreply.github.com>
2026-03-05 20:40:04 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:39:53.957Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:40:03.869Z
+Commit: Fix memory leak in event handlers
