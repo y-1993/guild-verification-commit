945e705bbbe4874241dfd2d738540597edd2e031
y-1993 <155223521+y-1993@users.noreply.github.com>
2026-03-05 20:39:55 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:39:23.979Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T17:39:55.156Z
+Commit: Add multi-language support
